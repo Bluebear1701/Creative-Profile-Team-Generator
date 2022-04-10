63481785ed5a91d6744155093d97eddb2b54d649
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-10 16:53:01 -0500

    add index and questions

---
+++ index.js
@@ -0,0 +1,47 @@
+const inquirer = require("inquirer");
+const questions = [
+    {
+        type: "list",
+        name: "choice",
+        message: "What kind of employee do you want to add?",
+        choices: ["Employee", "Intern", "Manager", "Engineer"],
+    }
+];
+
+const internquestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of this Intern?",        
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",        
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the name of the intern's school?",
+    }
+]
+function askuser() {
+    inquirer.prompt (questions).then(function(answers){
+        console.log (answers);
+        if (answers.choice === "Intern") {
+            createintern();
+        }
+    })
+} 
+function createintern() {
+    inquirer.prompt (internquestions).then(function(answers){
+        console.log (answers) 
+    })
+}
+askuser();
